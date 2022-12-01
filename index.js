2228fbaf9aec94d8f7da718a1c390b4830b322a7
Farhat Mahi <farhatmahi007@gmail.com>
2022-12-01 07:03:55 +0600

    get all products api added

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const app = express()
+const cors = require('cors')
+
+
+//middleware
+app.use(cors())
+app.use(express.json())
+
+
+//PORT
+const port = process.env.PORT || 1000;
+
+
+
+//mongodb
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://fable:fable@cluster0.6ulnnbw.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+//collections
+const allProductsCollection = client.db('fable').collection('allProducts')
+
+const run = async() => {
+    //get all products
+    app.get('/all-products', async(req, res) => {
+        const query = {}
+        const products = await allProductsCollection.find(query).toArray()
+        res.send(products)
+    })
+
+}
+run().catch(err => console.log(err))
+
+
+
+
+
+//app running 
+app.get('/', (req, res) => {
+    res.send("Fable server is running")
+})
+
+
+//app listener
+app.listen(port, () => {
+    console.log(`Fable is running on ${port}`)
+})
