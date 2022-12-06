a7dbf563cde16bc59c9afb8d7ab58f410b47c555
Farhat Mahi <farhatmahi007@gmail.com>
2022-12-06 08:38:10 +0600

    cart api

--- index.js
+++ index.js
@@ -1,81 +1,108 @@
-const express = require("express")
-const app = express()
-const cors = require('cors')
-
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
 //middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 //PORT
 const port = process.env.PORT || 1000;
 
-
-
 //mongodb
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://fable:fable@cluster0.6ulnnbw.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri =
+  "mongodb+srv://fable:fable@cluster0.6ulnnbw.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 //collections
-const allProductsCollection = client.db('fable').collection('allProducts')
-const userCollection = client.db('fable').collection('users')
+const allProductsCollection = client.db("fable").collection("allProducts");
+const userCollection = client.db("fable").collection("users");
+const cartCollection = client.db("fable").collection("cart");
 
-const run = async() => {
-    //get all products
-    app.get('/all-products', async(req, res) => {
-        const query = {}
-        const products = await allProductsCollection.find(query).toArray()
-        res.send(products)
-    })
+const run = async () => {
+  //get all products
+  app.get("/all-products", async (req, res) => {
+    const query = {};
+    const products = await allProductsCollection.find(query).toArray();
+    res.send(products);
+  });
 
-    //get products by category
-    app.get('/all-products/:type', async(req, res) => {
-        const type = req.params.type;
-        const query = {product_type : type}
-        const result = await allProductsCollection.find(query).toArray()
-        res.send(result)
-    })
+  //get products by category
+  app.get("/all-products/:type", async (req, res) => {
+    const type = req.params.type;
+    const query = { product_type: type };
+    const result = await allProductsCollection.find(query).toArray();
+    res.send(result);
+  });
 
-    //get product by id
-    app.get('/product/:id', async(req, res) => {
-        console.log(req);
-        const id = req.params.id;
-        const query = { _id : ObjectId(id)}
-        console.log(query);
-        const result = await allProductsCollection.findOne(query)
-        res.send(result)
-    })
+  //get product by id
+  app.get("/product/:id", async (req, res) => {
+    // console.log(req);
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    // console.log(query);
+    const result = await allProductsCollection.findOne(query);
+    res.send(result);
+  });
 
-    app.get('/users', async(req, res) => {
-        const query = {}
-        const users = await userCollection.find(query).toArray()
-        res.send(users)
-      })
+  app.get("/users", async (req, res) => {
+    const query = {};
+    const users = await userCollection.find(query).toArray();
+    res.send(users);
+  });
 
-    //post : users
-    app.post('/users', async(req, res) => {
-        const user = req.body;
-        const result = await userCollection.insertOne(user)
-        res.send(result)
-    })
+  app.get("/cart", async (req, res) => {
+    const query = {};
+    const result = await cartCollection.find(query).toArray();
+    res.send(result);
+  });
 
-}
-run().catch(err => console.log(err))
+  app.get("/cartFilteredByUser", async (req, res) => {
+    const email = req.query.email;
+    const query = { email: email };
+    const result = await cartCollection.find(query).toArray();
+    res.send(result);
+  });
+
+  //post : users
+  app.post("/users", async (req, res) => {
+    const user = req.body;
+    const result = await userCollection.insertOne(user);
+    res.send(result);
+  });
+
+  //adding to cart
+  app.post("/cart", async (req, res) => {
+    const cart = req.body;
+    const query = {
+      email: cart.email,
+      product_name: cart.product_name,
+    };
+
+    const alreadyInCart = await cartCollection.find(query).toArray();
+    if (alreadyInCart.length) {
+      const message = `Product already added on cart`;
+      return res.send({ acknowledged: false, message });
+    }
+    const result = await cartCollection.insertOne(cart);
+    res.send(result);
 
 
+  });
+};
+run().catch((err) => console.log(err));
 
-
-
-//app running 
-app.get('/', (req, res) => {
-    res.send("Fable server is running")
-})
-
+//app running
+app.get("/", (req, res) => {
+  res.send("Fable server is running");
+});
 
 //app listener
 app.listen(port, () => {
-    console.log(`Fable is running on ${port}`)
-})
+  console.log(`Fable is running on ${port}`);
+});
